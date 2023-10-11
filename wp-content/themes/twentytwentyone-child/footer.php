5ea30180dbd2150ae677213f06b1e5ed8a332bbe
devavology <145644105+devavology@users.noreply.github.com>
2023-10-11 16:22:44 +0530

    update footer.php

--- wp-content/themes/twentytwentyone-child/footer.php
+++ wp-content/themes/twentytwentyone-child/footer.php
@@ -127,7 +127,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 	   <script src="<?php echo get_stylesheet_directory_uri();?>/js/jquery.parallax.js"></script>
-	   <script src="js/cdn.jsdelivr.net_particles.js_2.0.0_particles.min.js"></script>
+	   <script src="<?php echo get_stylesheet_directory_uri();?>/js/cdn.jsdelivr.net_particles.js_2.0.0_particles.min.js"></script>
        <script>
          $(document).ready(function(){
            $('.links').click(function(){
@@ -363,7 +363,7 @@ particlesJS("particles-js", {
         "enable": true,
         "mode": "grab"
       },
-      "onclick": {
+      "onclick": { 
         "enable": true,
         "mode": "push"
       },
@@ -423,6 +423,84 @@ particlesJS("particles-js", {
         }
     }
 })
+      </script>
+	  <script>
+         $(document).ready(function(){
+         
+         var current_fs, next_fs, previous_fs; //fieldsets
+         var opacity;
+         var current = 1;
+         var steps = $("fieldset").length;
+         
+         setProgressBar(current);
+         
+         $(".next").click(function(){
+         
+         current_fs = $(this).parent();
+         next_fs = $(this).parent().next();
+         
+         //Add Class Active
+         $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+         
+         //show the next fieldset
+         next_fs.show();
+         //hide the current fieldset with style
+         current_fs.animate({opacity: 0}, {
+         step: function(now) {
+         // for making fielset appear animation
+         opacity = 1 - now;
+         
+         current_fs.css({
+         'display': 'none',
+         'position': 'relative'
+         });
+         next_fs.css({'opacity': opacity});
+         },
+         duration: 500
+         });
+         setProgressBar(++current);
+         });
+         
+         $(".previous").click(function(){
+         
+         current_fs = $(this).parent();
+         previous_fs = $(this).parent().prev();
+         
+         //Remove class active
+         $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+         
+         //show the previous fieldset
+         previous_fs.show();
+         
+         //hide the current fieldset with style
+         current_fs.animate({opacity: 0}, {
+         step: function(now) {
+         // for making fielset appear animation
+         opacity = 1 - now;
+         
+         current_fs.css({
+         'display': 'none',
+         'position': 'relative'
+         });
+         previous_fs.css({'opacity': opacity});
+         },
+         duration: 500
+         });
+         setProgressBar(--current);
+         });
+         
+         function setProgressBar(curStep){
+         var percent = parseFloat(100 / steps) * curStep;
+         percent = percent.toFixed();
+         $(".progress-bar")
+         .css("width",percent+"%")
+         }
+         
+         $(".submit").click(function(){
+         return false;
+         })
+         
+         });
       </script>
 </body>
 </html>
