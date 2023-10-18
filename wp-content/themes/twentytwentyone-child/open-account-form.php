083a9c2ad5464adc8361e37503c5baed62aedf89
devavology <145644105+devavology@users.noreply.github.com>
2023-10-18 16:21:05 +0530

    update open-account-form

--- wp-content/themes/twentytwentyone-child/open-account-form.php
+++ wp-content/themes/twentytwentyone-child/open-account-form.php
@@ -10,14 +10,13 @@ Template Name: open-account form
            <div class="row align-items-center justify-content-center">
             <div class="col-sm-12 col-md-12 col-lg-12 outer">
                      <div class="heading">
-                        <h2>Join 9+ million investors & traders</h2>
-                        <p>Open a trading and Demat account online and start investing for free</p>
+                       <?php the_field('open-account-heading'); ?>
                      </div>
                   </div>
               <div class="col-md-12 col-lg-10 col-sm-12">
                 <div class="search_right">
                         <div class="search_heading">
-                           <h2>Signup now</h2>
+                           <h2><?php the_field('search-heading	');?></h2>
                         </div>
                         <?php the_field('account-form'); ?>
                      </div>
