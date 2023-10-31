c2fc0b869131f841e70143e1f3d3b32b5e1f8d8a
devavology <145644105+devavology@users.noreply.github.com>
2023-10-31 10:30:18 +0530

    update footer.php

--- wp-content/themes/twentytwentyone-child/footer.php
+++ wp-content/themes/twentytwentyone-child/footer.php
@@ -502,5 +502,23 @@ particlesJS("particles-js", {
          
          });
       </script>
+	  
+<script>
+$(document).ready(function() {
+    // Get the current URL
+    var currentUrl = window.location.href;
+
+    // Loop through each anchor tag in the menu
+    $('#menu a').each(function() {
+        var targetUrl = $(this).attr('href');
+
+        // Compare the current URL to the anchor's href
+        if (currentUrl.indexOf(targetUrl) !== -1) {
+            // If they match, add the "active" class
+            $(this).addClass('actives');
+        }
+    });
+});
+</script>	  
 </body>
 </html>
